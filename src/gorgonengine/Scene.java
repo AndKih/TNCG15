0d3cd34adf8299e773e6d28355a4c531c8dc9001
valwa <valdemar.wadstromer@gmail.com>
2016-09-20 12:06:02 +0200

    nothing

--- src/gorgonengine/Scene.java
+++ src/gorgonengine/Scene.java
@@ -169,9 +169,9 @@ public class Scene {
         
         objects[2] = new Sphere(new ColorDbl(400000000, 200000000, 600000000), new Vertex(11, -2, 1), 1);
         
-        lights = new PointLightSource[1];
-        lights[0] = new PointLightSource(new Vertex(2,-3,-1),1.0);
-//        lights[1] = new PointLightSource(new Vertex(9,4,2),0.7);
+        lights = new PointLightSource[2];
+        lights[0] = new PointLightSource(new Vertex(7,-4,-1), 1.0);
+        lights[1] = new PointLightSource(new Vertex(2, 4, 4), 1.0);
         
         
         
