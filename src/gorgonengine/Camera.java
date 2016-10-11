7f46b0cf6d56fe29c41d54d9db5316f102cf284a
valwa <valdemar.wadstromer@gmail.com>
2016-10-11 12:13:22 +0200

    class

--- src/gorgonengine/Camera.java
+++ src/gorgonengine/Camera.java
@@ -33,7 +33,7 @@ public class Camera extends JFrame{
     public static final double IMPORTANCETHRESHOLD = 0.1;
     Scene scene;
     
-    public int raysPerPixel = 10;
+    public int raysPerPixel = 5;
     
     private double deltax;
     private double deltay;
@@ -66,9 +66,16 @@ public class Camera extends JFrame{
         double newY;
         double newZ;
                 
+        int percentEffictivicer = 0;
         for(int px = 0; px<SIZEX; px++)
         {
-//            System.out.println("Rays are: " + ((double)px/SIZEX)*100 +"% done");
+            //if statement just here to simplify waiting by displaying a number
+            if(((double)(px+1)/SIZEX)*100 >= percentEffictivicer)
+            {
+                System.out.println("Rays are: " + percentEffictivicer +"% done");
+                percentEffictivicer++;
+            }
+            
             for(int py = 0; py<SIZEY; py++)
             {
                     cam[px][py] = new Pixel(new ColorDbl(), px + py*SIZEX);
