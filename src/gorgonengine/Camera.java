a3c1ac84284f3dc90a3ed60215c7bf444f76eacd
valwa <valdemar.wadstromer@gmail.com>
2016-11-01 18:59:46 +0100

    Final

--- src/gorgonengine/Camera.java
+++ src/gorgonengine/Camera.java
@@ -34,14 +34,14 @@ public class Camera extends JFrame{
     public Vertex pos;
     public double width, height;
     private double iMax, iMin;
-    public static final double IMPORTANCETHRESHOLD = 0.05;
+    public static final double IMPORTANCETHRESHOLD = 0.1;
     public static final int N_REFLECTEDRAYS = 1;
-    public static final int N_AREALIGHTSOURCEPOINTS = 30;
+    public static final int N_AREALIGHTSOURCEPOINTS = 15;
     public static final int ESTIMATOR_ITERATIONS = 100;
     Scene scene;
     
     
-    public int raysPerPixel = 75;
+    public int raysPerPixel = 10;
     public static boolean areaLightsource = true;
     public static boolean logScale = false;
     
